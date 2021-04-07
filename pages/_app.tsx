cf83b009851fac234ebf09ab70a56eda83e216b0
Hoshi Ran <ureshinostyle350@gmail.com>
2021-04-07 17:53:02 +0900

    test typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+// After: _app.tsx
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/api/hello.ts
@@ -0,0 +1,6 @@
+// After: hello.ts
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ name: "John Doe" });
+};
