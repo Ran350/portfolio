3963206e019dbfa5686b251fb0b55500e4dcdac8
Hoshi Ran <ureshinostyle350@gmail.com>
2021-04-14 19:11:57 +0900

    [add] _document.js

---
+++ src/pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta charSet="UTF-8" />
+        </Head>
+        <body className="font-sans">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
