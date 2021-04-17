d7c8da536af787392d202d567d2493e0bbb4f8f2
Hoshi Ran <ureshinostyle350@gmail.com>
2021-04-17 19:50:48 +0900

    [add] classNames

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,6 @@
 import { FC } from "react";
+import ClassNames from "classnames";
+import classNames from "classnames";
 
 type Props = {
   isHoverShadow?: boolean;
@@ -6,10 +8,9 @@ type Props = {
 
 export const Card: FC<Props> = ({ children, isHoverShadow = false }) => (
   <div
-    className={
-      "bg-white m-2 border-transparent p-4 shadow" +
-      (isHoverShadow && "transition hover:shadow-2xl duration-500 ease-out")
-    }
+    className={classNames("bg-white m-2 border-transparent p-4 shadow", {
+      "transition hover:shadow-2xl duration-500 ease-out": isHoverShadow,
+    })}
   >
     {children}
   </div>
