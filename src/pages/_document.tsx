b320abcbc7338a27951d4e7ae7e20a059e4e740b
Hoshi Ran <ureshinostyle350@gmail.com>
2021-08-21 22:10:37 +0900

    [add] link tags for favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,12 +23,42 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <meta charSet="UTF-8" />
-
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Telex&display=swap"
             rel="stylesheet"
           />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color="#000000"
+          />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta
+            name="msapplication-config"
+            content="/favicons/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
 
           {/* Google Analytics */}
           {GA_TRACKING_ID && (
