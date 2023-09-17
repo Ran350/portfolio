cd9854ea1b85a81085f5d147c5bdff790f683056
Hoshi Ran <ureshinostyle350@gmail.com>
2023-09-18 03:33:47 +0900

    外部サイトのブログをブログ一覧に表示させるよう機能追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,6 +28,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Telex&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          />
 
           <link
             rel="apple-touch-icon"
---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,71 @@
+import { readFileSync } from "fs";
+
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import parseMD from "parse-md";
+import { FC } from "react";
+import ReactMarkdown from "react-markdown";
+import { z } from "zod";
+
+import { ReactMarkdownComponents } from "@/components/blog/ReactMarkdownComponents";
+import { Layout } from "@/components/Layout";
+import { blogs } from "@/const/blog";
+
+type Props = {
+  metadata: {
+    publishedAt: `${number}-${number}-${number}`;
+    title: string;
+    tags: string[];
+  };
+  content: string;
+};
+
+const BlogMeta: FC<{
+  title: string;
+}> = (props) => (
+  <div>
+    <h1 className="text-3xl font-bold">{props.title}</h1>
+  </div>
+);
+
+const BlogPage: NextPage<Props> = (props) => (
+  <Layout>
+    <BlogMeta {...props.metadata} />
+
+    <ReactMarkdown components={ReactMarkdownComponents}>
+      {props.content}
+    </ReactMarkdown>
+  </Layout>
+);
+export default BlogPage;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = blogs.map((blog) => ({
+    params: {
+      slug: blog.slug,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = z.object({ slug: z.string() }).parse(params);
+
+  // FIXME: blog/[slug] ページ毎に O(N) かかるので改善したい
+  const blog = blogs.find((blog) => blog.slug === slug);
+  if (blog === undefined) {
+    throw new Error(`Invalid slug: ${slug}`);
+  }
+
+  const fileContents = readFileSync(blog.path, "utf8");
+  const { content } = parseMD(fileContents);
+
+  return {
+    props: {
+      content,
+      metadata: { ...blog },
+    },
+  };
+};
