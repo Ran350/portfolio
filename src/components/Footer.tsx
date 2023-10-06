51ca1ada3d1a99937e5920506128b301ebeda889
Hoshi Ran <ureshinostyle350@gmail.com>
2023-10-07 00:13:34 +0900

    footer にナビゲーションを追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,12 @@
-// import Link from "next/link";
+import Link from "next/link";
 
 export const Footer = () => (
   <footer className="h-16 flex items-center bg-gray-50">
     <div className="w-full grid grid-cols-5">
       <h1 className="col-start-3 text-center">©2023 Ran350</h1>
       <div className="col-start-5 flex gap-8">
-        {/* <Link href="/">about</Link> */}
-        {/* <Link href="/blog">blog</Link> */}
-        {/* <Link href="/gallery">gallery</Link> */}
+        <Link href="/">about</Link>
+        <Link href="/article">article</Link>
       </div>
     </div>
   </footer>
