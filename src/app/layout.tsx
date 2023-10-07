08efd20bc79edfccf2dc281475930e87edf5fc72
Hoshi Ran <ureshinostyle350@gmail.com>
2023-10-07 17:56:08 +0900

    app router に移行した

---
+++ src/app/layout.tsx
@@ -0,0 +1,64 @@
+import { ReactNode } from "react";
+
+import "../styles/globals.css";
+
+// import { GoogleAnalyticsScript } from "@/feature/googleAnalytics/GoogleAnalyticsScript";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="ja">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Telex&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+
+        {/* favicon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-config"
+          content="/favicons/browserconfig.xml"
+        />
+
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* <GoogleAnalyticsScript /> */}
+      </head>
+
+      <body>{children}</body>
+    </html>
+  );
+}
